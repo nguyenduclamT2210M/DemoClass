62da5cdc05b717a9f5c5a11d3ecc3ddf613710f4
nguyenduclam <nguyenduclam048@gmail.com>
2023-03-20 10:08:29 +0700

    Initial commit

---
+++ src/com/class1/MainClass.java
@@ -0,0 +1,17 @@
+package com.class1;
+
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person person2 = new Person();
+
+        //Create objects
+        Person person3 = new Person();
+        Person person4 = new Person();
+
+        person1.inputData();
+        person1.display();
+    }
+}
